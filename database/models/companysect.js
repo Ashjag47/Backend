dd930261a9afe1f29afc09a433c94a29c957f09b
Ashjag47 <Ashish_P@mckinsey.com>
2023-02-03 15:42:52 +0530

    Feat: add endpoint to get company by id and sector

--- database/models/companysect.js
+++ database/models/companysect.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 const {
-	Model
-} = require("sequelize");
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-	class companySect extends Model {
-		/**
+  class companySect extends Model {
+    /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-		static associate(models) {
-			// define association here
-		}
-	}
-	companySect.init({
-		compId: DataTypes.STRING,
-		compSect: DataTypes.STRING
-	}, {
-		sequelize,
-		modelName: "companySect",
-	});
-	return companySect;
+    static associate(models) {
+      // define association here
+    }
+  }
+  companySect.init({
+    compId: DataTypes.STRING,
+    compSect: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'companySect',
+  });
+  return companySect;
 };
