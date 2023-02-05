f418df16f9434347a886eb6ce9dbcdaf4190673b
Ashjag47 <Ashish_P@mckinsey.com>
2023-02-05 19:16:47 +0530

    Feat: Add errors handling function to handle all errors

--- src/controllers/saveCompInfo.js
+++ src/controllers/saveCompInfo.js
@@ -1,23 +1,24 @@
 const {saveCompInfoService} = require("../service/saveCompInfo");
+const {HTTPError} = require('../utils/errors');
 
 const saveCompInfo = async(req, res) => {
 	console.log("POST api/save controller is called");
-	//try{
+	try{
 		const company= await saveCompInfoService(req.body.urlLink);
 		if (company==null) {
 			throw new HTTPError("company not found", 404);
 		}
 		res.status(200).json(company);
-	// } 
-	// catch (err) {
-	// 	if(err instanceof HTTPError){
-	// 		res.status(err.code).json({message: err.message});
-	// 	}
-	// 	else{
-	// 		console.log(err);
-	// 		res.status(500).json({ error: "Something went wrong" });
-	// 	}
-	// }
+	} 
+	catch (err) {
+		if(err instanceof HTTPError){
+			res.status(err.code).json({message: err.message});
+		}
+		else{
+			console.log(err);
+			res.status(500).json({ error: "Something went wrong" });
+		}
+	}
 
 };
 
